27dba2e166877691569acba0ae019f115898997c
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2022-11-27 22:27:41 -0300

    Implementa a estrutura básica da classe blockchain

--- module-1-blockchain/blockchain.py
+++ module-1-blockchain/blockchain.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 27 19:51:37 2022
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
 
-@author: bruno
-"""
+# Parte 1, criar uma blockchain
 
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+    def create_block(self, proof, previous_hash):
+        # cria um novo dicionario para o bloco
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        # adiciona o novo block a blockchain
+        self.chain.append(block)
+        return block
